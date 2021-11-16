e7f28f3a82c88a26d9396db9161ca2bbbdf3fc8c
Mariamekh <60445999+Mariamekh@users.noreply.github.com>
2021-11-16 18:05:04 +0400

    refactor: change routing example

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -10,7 +10,7 @@ const Header = () => {
             <NavLink to='/'>Counter</NavLink>
           </li>
           <li style={{ listStyle: "none", paddingRight: "50px" }}>
-            <NavLink to='/user'>User List</NavLink>
+            <Link to='/user'>User List</Link>
           </li>
           <li style={{ listStyle: "none", paddingRight: "50px" }}>
             <NavLink to='/todo'>Todo List</NavLink>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );

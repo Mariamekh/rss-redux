4d0f70508108a6b6ceb9e2b16d3871404d6d006b
Mariamekh <60445999+Mariamekh@users.noreply.github.com>
2021-11-16 17:33:25 +0400

    refactor: change browerrouter location

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React from "react";
 import UserList from "./components/UserList";
 import TodoList from "./components/TodoList";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Counter from "./components/Counter";
 import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Counter />} />
-          <Route path='/user' element={<UserList />} />
-          <Route path='/todo' element={<TodoList />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Counter />} />
+        <Route path='/user' element={<UserList />} />
+        <Route path='/todo' element={<TodoList />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
   );
 };
 
